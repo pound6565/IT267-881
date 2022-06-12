91ca831090c1dceb937c5e5c05fb777816df3223
pound6565 <644708020008@dpu.ac.th>
2022-06-12 11:57:47 +0700

    switch

---
+++ switch_procedure.py
@@ -0,0 +1,16 @@
+switch_status = False #สถานะปิด
+
+def turn_on():  #ฟังก์ชันเปิดไฟ
+    global switch_status
+    switch_status = True 
+    print("light on")
+
+def turn_off(): #ฟังก์ชันปิดไฟ
+    global switch_status
+    switch_status = False
+    print("light off")
+
+if __name__ == "__main__":
+    print(f'สถานะไฟ: {switch_status}')
+    turn_on()
+    turn_off()
