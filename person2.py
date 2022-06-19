acce3bc80d2d3acd1e7c58a2dd4c424099c6a7b4
pound6565 <644708020008@dpu.ac.th>
2022-06-19 09:37:07 +0700

    person2

--- person2.py
+++ person2.py
@@ -7,25 +7,27 @@ class Person:
 
     def work(self):
         print(f'{self.name} working as a {self.profession}')
-
-    def study(self,hours):
-        self.study = hours
     
     def show(self):
         print(f'Name: {self.name} Gender: {self.gender} Profession: {self.profession} Study:{self.study}')
 
-#person_1
-jessa = Person('Jessa','Female','Software Engineer',10)
-jessa.work()
-jessa.show()
+    def __del__(self):
+        print("object was destroyed")
 
-#person_2
-jon = Person('Jon','Male','Doctor',15)
-jon.work()
-jon.show()
+if __name__ =="__main__":
 
-#lisa
-lisa = Person('Lisa','Male','Korean Singer',10)
-lisa.work()
-lisa.show()
+    #person_1
+    jessa = Person('Jessa','Female','Software Engineer',10)
+    jessa.work()
+    jessa.show()
+
+    #person_2
+    jon = Person('Jon','Male','Doctor',15)
+    jon.work()
+    jon.show()
+
+    #lisa
+    lisa = Person('Lisa','Male','Korean Singer',10)
+    lisa.work()
+    lisa.show()
 
