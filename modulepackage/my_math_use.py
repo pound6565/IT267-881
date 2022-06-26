75ba0fd2be845dd4e5f85288dc89591443ea3177
pound6565 <644708020008@dpu.ac.th>
2022-06-26 09:58:37 +0700

    my

--- modulepackage/my_math_use.py
+++ modulepackage/my_math_use.py
@@ -5,7 +5,7 @@ print(f'sqart of 5 = {sqrt(5)}')
 print(f'circle area = {circle_area(2):,.2f}')
 
 #วิธี่ที่ 2
-import my_math
+import my_math as my
 print("****วิธีที่ 2****")
-print(f'sqart of 5 = {sqrt(5)}')
-print(f'circle_area = {circle_area(2):,.2f}')
+print(f'sqart of 5 = {my.sqrt(5)}')
+print(f'circle_area = {my.circle_area(2):,.2f}')
