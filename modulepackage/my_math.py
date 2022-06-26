7f840bee01df9ecf1d0e9e92550308ebd0bafd5e
pound6565 <644708020008@dpu.ac.th>
2022-06-26 09:54:53 +0700

    my_math

---
+++ modulepackage/my_math.py
@@ -0,0 +1,10 @@
+PI = 3.1416
+
+def sqrt(numder):
+    return (numder ** 2)
+
+def circle_area(radius):
+    return PI * (radius ** 2)
+
+
+ 
---
+++ modulepackage/my_math_use.py
@@ -0,0 +1,11 @@
+# วิธีที่ 1
+from my_math import sqrt,circle_area
+
+print(f'sqart of 5 = {sqrt(5)}')
+print(f'circle area = {circle_area(2):,.2f}')
+
+#วิธี่ที่ 2
+import my_math
+print("****วิธีที่ 2****")
+print(f'sqart of 5 = {sqrt(5)}')
+print(f'circle_area = {circle_area(2):,.2f}')
